9bf0ac756655ee569d27c1c47b35e1b565a124a7
MarcosMarfeo <127261535+MarcosMarfeo@users.noreply.github.com>
2023-03-07 20:46:02 -0300

    2 commit carpeta vendor

---
+++ composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '08faa862c5d6ec58aa27c0b9347ffba0c1b9b8b2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '08faa862c5d6ec58aa27c0b9347ffba0c1b9b8b2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
